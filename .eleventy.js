90cfd992ee11c87ffdeb5400d0948205d078911c
JC Olivares <juancri@juancri.com>
2023-10-29 01:51:52 -0300

    Using eleventy-plugin-git-commit-date

--- .eleventy.js
+++ .eleventy.js
@@ -5,12 +5,14 @@ const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginNavigation = require("@11ty/eleventy-navigation");
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
+const pluginGitCommitDate = require("eleventy-plugin-git-commit-date");
 
 module.exports = function(eleventyConfig) {
   // Add plugins
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(pluginSyntaxHighlight);
   eleventyConfig.addPlugin(pluginNavigation);
+  eleventyConfig.addPlugin(pluginGitCommitDate);
 
   // https://www.11ty.dev/docs/data-deep-merge/
   eleventyConfig.setDataDeepMerge(true);
